04b5eb4817cc7c68a701b290ca7918d8d0039b03
Marina <mvalpeters@gmail.com>
2022-08-05 19:43:15 +0300

    initial commit

---
+++ example002_hellouser/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("введите имя пользователя");
+string user_name = Console.ReadLine();
+Console.Write("привет, "); 
+Console.Write(user_name);
+
---
+++ example003_sum/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("Введите число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите еще число");
+int number2 = int.Parse(Console.ReadLine());
+int S = number1 + number2;
+Console.WriteLine ($"сумма = {S}");
