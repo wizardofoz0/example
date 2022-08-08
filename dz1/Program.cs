14b77e487e967543fec67f32c4c37fe41cd61a11
Marina <mvalpeters@gmail.com>
2022-08-08 20:48:01 +0300

    work with gitignore

---
+++ dz1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Console.WriteLine("Введите число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите еще число");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 > number2)
+{ 
+    Console.WriteLine ($" {number1} > {number2}");
+}
+else{
+
+    Console.WriteLine ($" {number1} < {number2}");
+}
+*/
+
+Console.WriteLine("Введите 3 числа");
+int number{3} = int.Parse(Console.ReadLine());
+int i ;
+for (i = 0, i= i+1; 3)
+
+if (number {i} > number {i+1} )
+{ 
+     int max = number {i} ;
+}
+else{
+
+    int max = number {i+1} ;;
+}
+Console.WriteLine ($" {max }")
