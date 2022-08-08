fcd9b659c34cf6d3d5162622e601324f7adb8192
Marina <mvalpeters@gmail.com>
2022-08-08 22:15:52 +0300

    zadanie 14

---
+++ zadanie12/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+int delenie(int number1, int number2)
+{
+    int result = number1 % number2;
+    return result;
+}
+
+Console.WriteLine("введите число 1");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("введите число 2");
+int number2 = int.Parse(Console.ReadLine());
+
+int digit = delenie (number1, number2);
+if (digit > 0) 
+{
+    Console.WriteLine($"Не кратно, остаток {digit}");
+}
+else 
+{
+    Console.WriteLine($"Кратно");
+}
---
+++ zadanie_14/Program.cs
@@ -0,0 +1,30 @@
+﻿int delenie(int number1)
+{
+    int result = number1 % 7;
+    int result1 = number1 % 23;
+    int r;
+    if ((result > 0) | (result1 > 0))
+    { 
+        r = 0;
+     //   Console.WriteLine(r);
+    }
+    else 
+    {
+        r = 1;
+     //    Console.WriteLine(r);
+    }
+    return r;
+}
+
+Console.WriteLine("введите число");
+int number1 = int.Parse(Console.ReadLine());
+
+int answer = delenie (number1);
+if (answer == 1) 
+{
+  Console.WriteLine("да");  
+}
+if (answer == 0) 
+{
+Console.WriteLine("нет");
+}
